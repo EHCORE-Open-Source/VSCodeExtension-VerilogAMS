20ae632e23d452e31a1bc86c10499ddff38444b0
Chao-Jen Huang <ephoton@hotmail.com>
2025-07-01 15:23:05 +0800

    Update the devEnvInstall.bat to setup the VSIX environment

--- devEnvInstall.bat
+++ devEnvInstall.bat
@@ -1,7 +1,7 @@
 @echo off
-npm install -g npm typescript ts-node yo generator-code eslint @vscode/vsce
-npm install @types/node @types/vscode@1.88.0
-npm install --save-dev vscode
-npm audit fix --force
-npm uninstall @types/mocha
+call npm install -g npm typescript ts-node yo generator-code eslint @vscode/vsce
+call npm install @types/node @types/vscode@1.88.0
+call npm install --save-dev vscode
+call npm audit fix --force
+call npm uninstall @types/mocha
 rd /s /q node_modules\@types\mocha
